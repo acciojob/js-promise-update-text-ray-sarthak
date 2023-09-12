ab92b8f83fda331acf14c6f311813035966a40b2
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-12 14:11:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let promise=new Promise((resolve) =>{
+	setTimeout(() =>{
+		resolve("Hello, world!")
+	},1000)
+});
+const element=document.getElementById("output");
+promise.then((data)=>{
+	element.innerHTML=data;
+})
+
